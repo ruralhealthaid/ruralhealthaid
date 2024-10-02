d57dfb232e00bdea4dbc73cce6484125d93ed572
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-10-02 13:20:56 +0000

    Feat: Created layout for mission and vision section

--- src/components/about/AboutUs.tsx
+++ src/components/about/AboutUs.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import MissionVission from "./MissionVission";
 
 const AboutUs = () => {
   return (
     <div>
       <Hero />
+      <MissionVission />
     </div>
   );
 };
---
+++ src/components/about/MissionVission.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MissionVission = () => {
+  return <div>MissionVission</div>;
+};
+
+export default MissionVission;
