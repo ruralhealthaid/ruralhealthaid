3efc49987ab574b9a887cece20cb0334e0ff2097
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-10-02 18:24:18 +0000

    Feat: Added Core values section

--- src/components/about/AboutUs.tsx
+++ src/components/about/AboutUs.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import MissionVission from "./MissionVision";
+import CoreValues from "./CoreValues";
 
 const AboutUs = () => {
   return (
     <div>
       <Hero />
       <MissionVission />
+      <CoreValues />
     </div>
   );
 };
