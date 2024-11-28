1d7215da834b60b63c61939b62b6567cc6d9a148
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-11-28 18:38:40 +0000

    Refactor: Made the program coordinator and team lead to come first and added Google console verification

--- src/components/about/Team.tsx
+++ src/components/about/Team.tsx
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react";
 import styles from "./Team.module.css";
 import salisuImg from "../assets/team/Salisu Nuhu.jpeg";
-import ruthImg from "../assets/team/Ruth Amoah.jpeg";
-import maryImg from "../assets/team/Ndabire Mary-Queen.jpeg";
+import maryImg from "../assets/team/Ruth Amoah.jpeg";
+import ruthImg from "../assets/team/Ndabire Mary-Queen.jpeg";
 import asaanaImg from "../assets/team/Asaana Muniratu.jpeg";
 
 const Team = () => {
@@ -13,19 +13,20 @@ const Team = () => {
         <h1>OUR TEAM</h1>
         <ul className={styles["team-members"]}>
           <li className={styles["team-member"]}>
-            <img src={salisuImg} className={styles.img} alt="team member 1" />
-            <div className={styles.text}>
-              <p className={styles.name}>Salisu Nuhu</p>
-              <p className={styles.designation}>Finance manager</p>
-            </div>
-          </li>
-          <li className={styles["team-member"]}>
             <img src={asaanaImg} className={styles.img} alt="team member 2" />
             <div className={styles.text}>
               <p className={styles.name}>Asaana Muniratu</p>
               <p className={styles.designation}>Program director</p>
             </div>
           </li>
+          <li className={styles["team-member"]}>
+            <img src={salisuImg} className={styles.img} alt="team member 1" />
+            <div className={styles.text}>
+              <p className={styles.name}>Salisu Nuhu</p>
+              <p className={styles.designation}>Finance manager</p>
+            </div>
+          </li>
+
           <li className={styles["team-member"]}>
             <img src={ruthImg} className={styles.img} alt="team member 3" />
             <div className={styles.text}>
