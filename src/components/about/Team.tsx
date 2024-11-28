450b08eb60108e20a0f60d35e6cb1bc165cf3efe
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-11-28 18:42:49 +0000

    Refactor: Removed 1 team member

--- src/components/about/Team.tsx
+++ src/components/about/Team.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import styles from "./Team.module.css";
 import salisuImg from "../assets/team/Salisu Nuhu.jpeg";
-import maryImg from "../assets/team/Ruth Amoah.jpeg";
 import ruthImg from "../assets/team/Ndabire Mary-Queen.jpeg";
 import asaanaImg from "../assets/team/Asaana Muniratu.jpeg";
 
@@ -32,13 +31,6 @@ const Team = () => {
             <div className={styles.text}>
               <p className={styles.name}>Ruth Amoah</p>
               <p className={styles.designation}>Health care coordinator</p>
-            </div>
-          </li>
-          <li className={styles["team-member"]}>
-            <img src={maryImg} className={styles.img} alt="team member 3" />
-            <div className={styles.text}>
-              <p className={styles.name}>Ndabire Mary-Queen</p>
-              <p className={styles.designation}>Designation</p>
             </div>
           </li>
         </ul>
